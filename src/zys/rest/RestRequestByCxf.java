b14d3715c6700927f9c76c33293abe385dc57c9b
zys-kirsten <1085086694@qq.com>
2016-08-10 09:06:02 +0800

    change to maven

---
+++ src/zys/rest/RestRequestByCxf.java
@@ -0,0 +1,20 @@
+package zys.rest;
+
+import com.sun.webkit.WebPageClient;
+
+public class  RestRequestByCxf{
+	private static String REST_URI=
+			"http://localhost:8080/activiti-rest/service/management/properties";
+	
+	
+	//private static WebPageClient createClient(String uri){
+		//WebPageClient client = WebPageClient.cr
+				
+	//}
+	
+	
+	public static void main(String[] args) {
+		//WebClient
+	}
+
+}
